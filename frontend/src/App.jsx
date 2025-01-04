1198ce899d82d81f4e7e6c9e53efb265b6228afb
Mitanshi Atyantik <i.mitanshi@atyantik.com>
2025-01-04 18:12:11 +0530

    Frontend Components till Login

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,14 +1,26 @@
 import './App.css'
+import {BrowserRouter , Routes, Route} from 'react-router-dom'
+import Banner from './components/Banner'
+import Footer from './components/Footer'
+import Freebook from './components/Freebook'
 import Navbar from './components/Navbar'
+import Home from './Home/Home'
+import Courses from './Course/Courses'
 
 function App() {
-
   return (
     <>
-    <Navbar/>
-
+      <BrowserRouter>
+        <Navbar />
+       <div className='dark:bg-slate-900 dark:text-white'>
+       <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/course" element={<Courses/>} />
+        </Routes>
+       </div>
+        <Footer />
+      </BrowserRouter>
     </>
   )
 }
-
 export default App
---
+++ frontend/src/Course/Courses.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import Course from '../components/Course'
+
+const Courses = () => {
+  return (
+    <div>
+      <Course/>
+    </div>
+  )
+}
+
+export default Courses
